6a524bf50385db6e6101640e06379b01f22ae76b
xz2014 <zhangxinyu@pc-135-129.customer.hammersmith.generation.lan>
2017-05-15 06:32:46 +0100

    a

--- hdp_gamma_sampler/hdp_gamma_sampler.py
+++ hdp_gamma_sampler/hdp_gamma_sampler.py
@@ -11,41 +11,39 @@ class hdp_GGPP:
 		self.beta = beta
 		self.V = V   
 		self.m = m
-		self.maxNIter = maxNIter # number of iterations to update n_dk in each iteration
 		self.T = T
-		self.numIter = numIter # total number of iterations 
-		self.sizeofstack = sizeofstack  # size of each stack, needs to be large so the empirical distribution of S_d is same as X_dk
-
-
-
 		self.corpus = corpus
+
 		self.D = len(corpus)
 		self.topics = range(T)
+		self.initial_topics = [[0]*len(corpus[0])]*2
 
+##### variables:
 		self.S_d = [np.random.choice(corpus[d],sizeofstack,replace=True).tolist() for d in range(D)] # stack for each document
 		self.B_dk = [[[] for k in topics] for a in corpus] # buffer
 		self.X_dk = [[[] for k in topics] for a in corpus] # each X_dk[d][k] is a list of words
 		self.n_dk =[np.array([0]*T) for a in corpus] # each n_dk[d][k] is the number of words in document d with topic k
-
-#### initialize
-
-		self.initial_topics = [[0]*len(corpus[0])]*2
 		self.alpha_k = [[] for a in topics]  
 		self.theta_k = [np.zeros(V,dtype=int) for a in topics]
 		self.pi_dk = [[np.ndarray(0,dtype=int) for k in topics] for a in corpus]
 
+#### initialize each variable
 
+	def initial_(self):
 		for d in range(D):
 			for k in range(T):
 				z = [x for x in corpus[d] if initial_topics[d][k] == k ]
 				X_dk[d][k].extend(z)
 				n_dk[d][k] = len(z)
 
+#### for a given d and k, update n_dk using metroplis hasting algorithm.
 
-		
+	def updating_ndk(self, d, k, maxNIter):
+		for iter in range(1,maxNIter):
+			newn =sample_ndk(d,k, maxBufferSize)
+		return(newn)
 
-
-
+##### 
 
 	def inference(self,numIter):
 		for k in range(self.T):
@@ -58,7 +56,7 @@ class hdp_GGPP:
 
 
 
-	def parallel_for_each_topic(self,k,maxNIter,maxBufferSize):
+	def parallel_for_each_topic(self,k,maxNIter,maxBufferSize): # maxNIter: number of iterations to update n_dk in each iteration
 		n_new=[]
 		for d in range(D):
 			for niter in range(maxNIter):
@@ -131,20 +129,7 @@ class hdp_GGPP:
 					nacceptMinus =+ 1
 					B_dk[d][k].append(x)
 				else:
-					X_dk[d][k].extend(x)
-			else:
-				x=np.random.choice(S_d[d],replace=False)
-				S_d[d].remove(x)
-
-				uu = np.random.uniform(0,1)
-
-				if (uu < p_plus(x)): 
-					n_dk[d][k] =+ 1
-					X_dk[d][k].extend(x)
-					nacceptAdd =+ 1
-				else:
-					B_dk[d][k].append(x)
-
+					X_dk[d][k].append(x)
 
 		if (len(B_dk[d][k]) > maxBufferSize):
 			S_d[d].extend(B_dk[d][k])
