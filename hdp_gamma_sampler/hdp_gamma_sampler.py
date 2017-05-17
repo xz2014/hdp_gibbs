4af8f7a5398ad2975ec109e43954cb940d80e55a
xz2014 <zhangxinyu@pc-135-129.customer.hammersmith.generation.lan>
2017-05-17 02:21:04 +0100

    a

--- hdp_gamma_sampler/hdp_gamma_sampler.py
+++ hdp_gamma_sampler/hdp_gamma_sampler.py
@@ -13,7 +13,7 @@ class hdp_GGPP:
 		self.alpha = alpha 
 		self.beta = beta   
 		self.V = V   
-		self.m = m   # n_dk is poisson ~ ( m * pi_dk ) , empirically set as 1/T. 
+		self.m = m  # n_dk is poisson ~ ( m * pi_dk ) , empirically set as 1/T. 
 		self.T = T   # number of topics
 		self.corpus = corpus
 		self.sizeofstack = sizeofstack # size of each stack for a document. 
@@ -24,13 +24,15 @@ class hdp_GGPP:
 		self.D = len(corpus)
 		self.topics = range(T)  # topics are indexed as 0,1,2...
 		self.initial_topics = [[0]*len(corpus[0])]*2  # initial topic assignment
+		# memoization
+		self.aT = alpha*1.0/T
 
 ##### All variables:
 		self.S_d = [np.random.choice(corpus[d],sizeofstack,replace=True).tolist() for d in range(self.D)] # the stack for each document
 		self.B_dk = [[[] for k in self.topics] for a in corpus] # buffer
 		self.X_dk = [[[] for k in self.topics] for a in corpus] # each X_dk[d][k] is a list of words
 		self.n_dk =[[0]*T for a in corpus] # each n_dk[d][k] is the number of words in document d with topic k
-		self.alpha_k = []*T
+		self.alpha_k = [1]*T
 		self.theta_k = [[0.0]*V for a in self.topics]
 		self.pi_dk = [[np.ndarray(0,dtype=int) for k in self.topics] for a in corpus]
 
@@ -45,7 +47,8 @@ class hdp_GGPP:
 		self.pi_dk = [[1]*self.T for a in self.corpus]
 		self.alpha_k = [1]*self.T
 		self.theta_k = [[0.5]*self.V for a in self.topics]
-		m = 1/self.T
+		self.m = 1*1.0/self.T
+
 
 
 
@@ -54,29 +57,31 @@ class hdp_GGPP:
 	def parallel_samplging_for_each_topic(self,k): 
 		self.initial_()
 		new_n_with_topic_k=[]
-		L=[]
+	#	L=[]
 
 		for t in range(1,self.numIter):
 			for d in range(self.D):
 				# update n_dk for each d and k
-				n_dk[d][k] = self.updating_ndk(d, k)
+				self.n_dk[d][k] = self.updating_ndk(d, k)
 				
 				# update pi_dk for each d and k
-				pi_dk[d][k] = np.random.gamma(n_dk[d][k]+alpha_k[k], m+1)
+				self.pi_dk[d][k] = np.random.gamma(self.n_dk[d][k]+self.alpha_k[k], self.m+1)
+
 
 			# update alpha_k for each k
-			alpha_k = np.random.gamma(self.alpha/self.T+self.D,-sum(np.log(zip(*pi_dk)[k])))
+			self.alpha_k[k] = self.sample_alpha_k(k)
 
 			# update theta_k for each k
-			prob_theta_k = np.exp(sum((self.beta+self.n_kw[k])*np.log(theta_k[k])))
-			theta_k = np.random.dirichlet(np.add(self.beta,self.n_kw[k]))
+			prob_theta_k = np.exp(sum((map(lambda x,y:x+y, self.beta,self.n_kw(self.X_dk)[k]))*np.log(self.theta_k[k])))
+			self.theta_k[k] = np.random.dirichlet(np.add(self.beta,self.n_kw(self.X_dk)[k]))
 
 			# compute log likelihood
 			l = self.log_likelihood()
-			L.append(l)
-			print "The Log-likelihood at iteration {}. is {}. Topic{}'.format(t,l,k)"
+	#		L.append(l)
 
-		return(L)
+			print "The Log-likelihood at iteration {} is {}. Topic{}".format(t,l,k)
+
+	#	return(L)
 
 
 	
@@ -94,66 +99,72 @@ class hdp_GGPP:
 		u = np.random.uniform(0,1)
 
 		if u<0.5:
-			x=np.random.choice(S_d[d],replace=False)
-			S_d[d].remove(x)
+			x=np.random.choice(self.S_d[d],replace=False)
+			self.S_d[d].remove(x)
 
 			uu = np.random.uniform(0,1)
 
-			if (uu < p_plus(x)): # accept with prob = min(1,p_plus)
-				n_dk[d][k] += 1
-				X_dk[d][k].append(x)
+			if (uu < self.p_plus(d,k,x)): # accept with prob = min(1,p_plus)
+				self.n_dk[d][k] += 1
+				self.X_dk[d][k].append(x)
 			else:
-				B_dk[d][k].append(x)
+				self.B_dk[d][k].append(x)
 
 		else:
-			if (len(X_dk[d][k]) > 0):
-				x=np.random.choice(X_dk[d][k],replace=False)
-				X_dk[d][k].remove(x)
+			if (len(self.X_dk[d][k]) > 0):
+				x=np.random.choice(self.X_dk[d][k],replace=False)
+				self.X_dk[d][k].remove(x)
 				uu = np.random.uniform(0,1)
-				if (uu < p_minus(x)):  
-					n_dk[d][k] -= 1
-					B_dk[d][k].append(x)
+				if (uu < self.p_minus(d,k,x)):  
+					self.n_dk[d][k] -= 1
+					self.B_dk[d][k].append(x)
 				else:
-					X_dk[d][k].append(x)
+					self.X_dk[d][k].append(x)
 
-		if (len(B_dk[d][k]) > maxBufferSize):
-			S_d[d].extend(B_dk[d][k])
+		if (len(self.B_dk[d][k]) > self.maxBufferSize):
+			self.S_d[d].extend(self.B_dk[d][k])
 
-		return(n_dk[d][k])
+		return(self.n_dk[d][k])
 
 
 
 
 # acceptance rate 
-	def p_minus(self,w):
-		return(n_dk[d][k]/((m*pi_dk[d][k])*theta_k[k][w]))
-		
+	def p_minus(self,d,k,w):
+		if  self.pi_dk[d][k]*self.theta_k[k][w] == 0 :
+			return (1.0)
+		else:
+			return(1.0*self.n_dk[d][k]/(self.m*self.pi_dk[d][k]*self.theta_k[k][w]))
 
-	def p_plus(self,w):
-		return((m*pi_dk[d][k])*theta_k[k][w]/(n_dk[d][k]+1))
+	def p_plus(self,d,k,w):
+		return((self.m*self.pi_dk[d][k])*self.theta_k[k][w]/(self.n_dk[d][k]+1))
 
-
+	def sample_alpha_k(self,k):
+		if -sum(np.log(zip(*self.pi_dk)[k])) > 0:
+			return(np.random.gamma(self.aT+self.D,-sum(np.log(zip(*self.pi_dk)[k]))))
+		else:
+			return(self.alpha_k[k])
 
 
 # complete loglikelihood log P(n_dk, pi_dk, alpha_k, theta_k, X_dk)
 	def log_likelihood(self):
 		LL=[]
-		for k in range(T):
-			v = np.array(zip(*n_dk)[k])
-			q = np.array(zip(*pi_dk)[k])
-			e = np.array(zip(*X_dk)[k])
-			l1 = sum(np.log(m)*v-sp.gammaln(alpha_k[k])+(v+alpha_k[k]-1)*np.log(q)-(m+1)*q-np.log(sm.factorial(v)))
-			l2 = (-1+alpha/T)*np.log(alpha_k[k]) - alpha_k[k] +sum(beta*np.log(theta_k[k]))
-			l3 = sum(self.n_kw(X_dk)[k]*np.log(theta_k[k]))
+		for k in range(self.T):
+			v = np.array(zip(*self.n_dk)[k])
+			q = np.array(zip(*self.pi_dk)[k])
+			e = np.array(zip(*self.X_dk)[k])
+			l1 = sum(np.log(self.m)*v-sp.gammaln(self.alpha_k[k])+(v+self.alpha_k[k]-1)*np.log(q)-(self.m+1)*q-np.log(sm.factorial(v)))
+			l2 = (-1+self.aT)*np.log(self.alpha_k[k]) - self.alpha_k[k] +sum(self.beta*np.log(self.theta_k[k]))
+			l3 = sum(self.n_kw(self.X_dk)[k]*np.log(self.theta_k[k]))
 			LL.append(l1+l2+l3)
 		return(sum(LL))
 
  # number of times word w assigned with topic k 
 	def n_kw(self,X_dk):
-		n_kw = [[0]*V for a in self.topics]
-		for k in range(T):
-			for w in range(V):
-				c = sum([a for a in zip(*X_dk)[k]],[]).count(w)
+		n_kw = [[0]*self.V for a in self.topics]
+		for k in range(self.T):
+			for w in range(self.V):
+				c = sum([a for a in zip(*self.X_dk)[k]],[]).count(w)
 				n_kw[k][w] = c
 		return(n_kw)
 
